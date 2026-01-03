85b081a0895174586be2e3419e43152ecb30c6c1
Дарья Дубровская <dariadubrovskay@gmail.com>
2026-01-03 18:27:16 +0200

    Edit alembic connection

--- alembic/env.py
+++ alembic/env.py
@@ -18,7 +18,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-#config.set_main_option("sqlalchemy.url", get_db_url())
+config.set_main_option("sqlalchemy.url", get_db_url())
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
